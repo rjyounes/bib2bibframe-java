49c099f13bfe39966b7c8698c40b004828b24c1b
Rebecca Younes <rjy7@lib-c02mjedqfd57.library.cornell.edu>
2015-06-10 15:34:19 -0400

    Move main method to another class for modularity

---
+++ src/shellcommand/Marcxml2BibframeShellCommand.java
@@ -0,0 +1,80 @@
+package shellcommand;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.ProcessBuilder.Redirect;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.io.FilenameUtils;
+
+public class Marcxml2BibframeShellCommand {
+      
+    public static String execute(String marcxmluri) {
+
+        String output = new String();
+        
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd-HHmmss");
+        String datetime = format.format(new Date());
+        // If supporting multiple RDF formats, "rdfxml" will vary depending
+        // on format.
+        // Simplify directory structure for debugging
+        // String parentName = System.getProperty("user.dir") + "/out/"; 
+        String parentName = System.getProperty("user.dir") + "/out/" + datetime + "/bibframe/rdfxml";
+        File parent = new File(parentName);
+        parent.mkdirs();
+        
+        String basename = FilenameUtils.getBaseName(marcxmluri);
+        File outputFile = new File(parent, basename + ".rdf");  
+        
+        try {       
+            
+            ProcessBuilder pb = new ProcessBuilder("/usr/bin/java",
+                    "-cp",
+                    "/Users/rjy7/Workspace/bib2bibframe-java/lib/saxon951/saxon9he.jar",
+                    "net.sf.saxon.Query",
+                    "/Users/rjy7/Workspace/bib2bibframe-java/lib/marc2bibframe/xbin/saxon.xqy",
+                    "marcxmluri=" + marcxmluri,
+                    "baseuri=http://ld4l.library.cornell.edu/individual/",
+                    "serialization=rdfxml"       
+                    );
+ 
+ 
+            
+            // Working directory; default is the current working directory of 
+            // the current process.
+            pb.directory();
+            
+            // Required even though we're just using the default environment
+            // (copy of the environment in the current process).
+            // Assign to a variable if we want to set values.
+            // Map<String, String> env = pb.environment();
+            pb.environment();
+            
+            pb.redirectOutput(Redirect.appendTo(outputFile));
+            //pb.redirectOutput(Process.getInputStream());
+            
+            // Process p = pb.start();
+            pb.start();
+            
+            // TODO Read in output file contents as String and return
+            // If we don't want the file, r Process.getInputStream()
+            // This should read the contents of outputFile into a String, but
+            // it doesn't.
+            //output = FileUtils.readFileToString(outputFile);
+            //outputFile.delete();
+            
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } 
+        
+        return output;
+
+    }
+    
+
+
+}
