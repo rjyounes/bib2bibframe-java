9ddb9aa1e03260baa5b45e8cc77ef630264bed11
Rebecca Younes <rjy7@lib-c02mjedqfd57.library.cornell.edu>
2015-06-10 13:45:18 -0400

    Replace java with ruby command
    It seems the java version wasn't working after all

--- src/shellcommand/ExecuteShellCommand.java
+++ src/shellcommand/ExecuteShellCommand.java
@@ -8,7 +8,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
 
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 
 public class ExecuteShellCommand {
     
@@ -27,31 +27,63 @@ public class ExecuteShellCommand {
     public static String execute(String marcxmluri) {
 
         String output = new String();
+        System.out.println(marcxmluri);
         
         try {       
           
-            ProcessBuilder pb = new ProcessBuilder("/usr/bin/java",
-                    "-cp",
-                    "/Users/rjy7/Workspace/javatest/lib/saxon951/saxon9he.jar",
-                    "net.sf.saxon.Query",
-                    "/Users/rjy7/Workspace/javatest/lib/marc2bibframe/xbin/saxon.xqy",
-                    "marcxmluri=" + marcxmluri,
-                    "baseuri=http://ld4l.library.cornell.edu/individual/",
-                    "serialization=rdfxml"       
+//            ProcessBuilder pb = new ProcessBuilder("/usr/bin/java",
+//                    "-cp",
+//                    "/Users/rjy7/Workspace/bib2bibframe-java/lib/saxon951/saxon9he.jar",
+//                    "net.sf.saxon.Query",
+//                    "/Users/rjy7/Workspace/bib2bibframe-java/lib/marc2bibframe/xbin/saxon.xqy",
+//                    "marcxmluri=" + marcxmluri,
+//                    "baseuri=http://ld4l.library.cornell.edu/individual/",
+//                    "serialization=rdfxml"       
+//                    );
+            
+            ProcessBuilder pb = new ProcessBuilder("/usr/bin/ruby",
+                    "/Users/rjy7/data/data-tools/marcxml2bibframe.rb",
+                    "--baseuri=http://ld4l.library.cornell.edu/individual/",
+                    "--targetdir=/Users/rjy7/Workspace/bib2bibframe-java/output",
+                    "--source=" + marcxmluri    
                     );
  
             DateFormat format = new SimpleDateFormat("yyyy-MM-dd-HHmmss");
-            File outputFile = new File(format.format(new Date())); 
-            pb.directory(new File("/Users/rjy7/Workspace/bib2bibframe-java"));
+            String outputDirName = format.format(new Date());
+            // If supporting multiple RDF formats, "rdfxml" will vary depending
+            // on format.
+            // String parentName = System.getProperty("user.dir") + "/out/" + outputDirName + "/bibframe/rdfxml";
+            String parentName = System.getProperty("user.dir") + "/out/"; // for debugging
+            File parent = new File(parentName);
+            parent.mkdirs();
+            
+            String basename = FilenameUtils.getBaseName(marcxmluri);
+            File outputFile = new File(parent, basename + ".rdf");   
+            
+            // Working directory; default is the current working directory of 
+            // the current process.
+            pb.directory();
+            //pb.directory(new File("/Users/rjy7/Workspace/bib2bibframe-java/"));
+            
+            // Required even though we're just using the default environment
+            // (copy of the environment in the current process).
+            // Assign to a variable if we want to set values.
             Map<String, String> env = pb.environment();
-            // TODO Redirect to input stream instead (Process.getInputStream();
+            //pb.environment();
+            
             pb.redirectOutput(Redirect.appendTo(outputFile));
-            Process p = pb.start();
+            //pb.redirectOutput(Process.getInputStream());
+            
+            // Process p = pb.start();
+            pb.start();
+            
             // TODO Read in output file contents as String and return
             // If we don't want the file, r Process.getInputStream()
             // This should read the contents of outputFile into a String, but
             // it doesn't.
-            output = FileUtils.readFileToString(outputFile);
+            //output = FileUtils.readFileToString(outputFile);
+            //outputFile.delete();
+            
 
         } catch (IOException e) {
             // TODO Auto-generated catch block
